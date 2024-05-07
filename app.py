7b25c113008c38cb91e6726a94fbc2b42891ea58
Silverflare29 <162567091+Silverflare29@users.noreply.github.com>
2024-05-07 15:19:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -31,5 +31,5 @@ else:
     st.image(image,use_column_width=True)
     prediction=import_and_predict(image,model)
     class_names=['Sunrise','Shine', 'Rain', 'Cloudy']
-    string="The weather is "+class_names[np.argmax(prediction)]+str(prediction*10000000)
+    string="The weather is "+class_names[np.argmax(prediction)]+str(round(prediction*100000,2))
     st.success(string)
