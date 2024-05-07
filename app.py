d908e4f32501cbf5b69bb583cd9d2e51373218b0
Silverflare29 <162567091+Silverflare29@users.noreply.github.com>
2024-05-07 15:49:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ else:
     image=Image.open(file)
     st.image(image,use_column_width=True)
     prediction=import_and_predict(image,model)
-    class_names=['Sunrise','Cloudy', 'Rain', 'Shine']
+    class_names=['Cloudy','Rain', 'Shine', 'Sunrise']
     prediction = prediction*100000
     prediction = np.array(prediction,dtype=int)
     prediction[0][2]=prediction[0][2]%100
